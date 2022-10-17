1268cd6aab7e9e835b179825d2c57905be75e6d0
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-10-17 14:32:24 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
+import { React, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState('');
+  const [result, setResult] = useState('');
+  const [operator, setOperator] = useState('');
+  const [isFinalResult, setIsFinalResult] = useState(false);
+
+  const handleNumberClick = (e) => {
+    let btnText = e.target.innerText;
+    if (isFinalResult) {
+      setNum(btnText);
+      setIsFinalResult(false);
+    } else {
+      setNum(num + btnText);
+    }
+  };
+
+  const handleDecimalPointClick = () => {
+    if (!num.includes('.')) {
+      setNum(num + '.');
+    }
+  };
+
+  const handleDelClick = () => {
+    if (num.length === 1) {
+      setNum('');
+    } else {
+      setNum(num.slice(0, -1));
+    }
+  };
+
+  const handleResetClick = () => {
+    setNum('');
+    setResult('');
+    setOperator('');
+    setIsFinalResult(false);
+  };
+
+  const handleOperatorClick = (e) => {
+    let operator = e.target.innerText;
+    setOperator(operator);
+    if (num) {
+      setResult(num);
+      setNum('');
+    } else {
+      setIsFinalResult(false);
+    }
+  };
+
+  const handleEqualsClick = () => {
+    let res = 0;
+
+    switch (operator) {
+      case '+':
+        res = parseFloat(result) + parseFloat(num);
+        break;
+      case '-':
+        res = parseFloat(result) - parseFloat(num);
+        break;
+      case '×':
+        res = parseFloat(result) * parseFloat(num);
+        break;
+      case '/':
+        res = parseFloat(result) / parseFloat(num);
+        break;
+      default:
+        break;
+    }
+
+    setResult(res.toString());
+    setOperator('');
+    setNum('');
+    setIsFinalResult(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='calc'>
+        <input
+          className='screen'
+          type='text'
+          value={isFinalResult ? result : num}
+          readOnly
+        />
+        <div className='buttons'>
+          <div className='row'>
+            <button className='button' onClick={handleNumberClick}>
+              7
+            </button>
+            <button className='button' onClick={handleNumberClick}>
+              8
+            </button>
+            <button className='button' onClick={handleNumberClick}>
+              9
+            </button>
+            <button className='button blue' onClick={handleDelClick}>
+              DEL
+            </button>
+          </div>
+          <div className='row'>
+            <button className='button' onClick={handleNumberClick}>
+              4
+            </button>
+            <button className='button' onClick={handleNumberClick}>
+              5
+            </button>
+            <button className='button' onClick={handleNumberClick}>
+              6
+            </button>
+            <button className='button' onClick={handleOperatorClick}>
+              +
+            </button>
+          </div>
+          <div className='row'>
+            <button className='button' onClick={handleNumberClick}>
+              1
+            </button>
+            <button className='button' onClick={handleNumberClick}>
+              2
+            </button>
+            <button className='button' onClick={handleNumberClick}>
+              3
+            </button>
+            <button className='button' onClick={handleOperatorClick}>
+              -
+            </button>
+          </div>
+          <div className='row'>
+            <button className='button' onClick={handleDecimalPointClick}>
+              .
+            </button>
+            <button className='button' onClick={handleNumberClick}>
+              0
+            </button>
+            <button className='button' onClick={handleOperatorClick}>
+              /
+            </button>
+            <button className='button' onClick={handleOperatorClick}>
+              ×
+            </button>
+          </div>
+          <div className='row'>
+            <button className='button blue' onClick={handleResetClick}>
+              RESET
+            </button>
+            <button className='button orange' onClick={handleEqualsClick}>
+              =
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
